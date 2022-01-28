9d8d63cf997bf09a4cb8d07b0466df4922094e5d
MFRasfan <rasfanfaiz@gmail.com>
2022-01-28 16:55:17 -0500

    new

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://MFR6461:BqrdkuWEtfoHYfCY@mfr.yspe9.mongodb.net/IAC?retryWrites=true&w=majority" ,
+  secretOrKey: "secret"
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const { join } = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(morgan("dev"));
+app.use(helmet());
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
